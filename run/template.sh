d68c339be0213f5e01db25fce9d9afe014b2f5e9
neotechz <marc.ayalde@gmail.com>
2025-07-18 12:53:45 +0800

    Add cutoff for FILTER_BY_POLYMORPHIC_SITES module to run/template.sh

--- run/template.sh
+++ run/template.sh
@@ -1,6 +1,7 @@
 nextflow run main.nf \
     --data "data" \
     --results "results" \
+    --filter_by_polymorphic_sites_cutoff null \
     --filter_by_nucleotide_diversity_start null \
     --filter_by_nucleotide_diversity_end null \
     --filter_by_dnds_ratio_start null \
