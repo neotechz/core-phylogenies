6787a064ed758f279a19116b4a8da381ba641ab9
neotechz <marc.ayalde@gmail.com>
2025-07-20 19:11:32 +0800

    Add dendropy from project Dockerfile and remove irrelevant packages

--- assets/core-phylogenies.Dockerfile
+++ assets/core-phylogenies.Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.13
 
 RUN pip install --no-cache-dir --upgrade pip && \
-    pip install --no-cache-dir pandas==2.2.3 matplotlib==3.10.0 biopython==1.85 scipy==1.16.0
+    pip install --no-cache-dir biopython==1.85 scipy==1.16.0 dendropy==5.0.8
 
 CMD ["cat", "/etc/os-release"]
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-docker build --file assets/core-phylogenies.Dockerfile --tag neotechz/core-phylogenies:1.0 .
+docker build --file assets/core-phylogenies.Dockerfile --tag neotechz/core-phylogenies:1.1 .
 docker pull quay.io/biocontainers/modeltest-ng:0.1.7--hf316886_3
 docker pull quay.io/biocontainers/raxml-ng:0.9.0--h192cbe9_1
 
