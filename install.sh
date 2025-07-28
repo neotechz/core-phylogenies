fa712d2ab5f8627cbbb6b54b0477b7dabd521b46
neotechz <marc.ayalde@gmail.com>
2025-07-28 13:23:58 +0800

    Add fasttree and iqtree2 to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 docker pull neotechz/core-phylogenies:1.1
 docker pull quay.io/biocontainers/modeltest-ng:0.1.7--hf316886_3
 docker pull quay.io/biocontainers/raxml-ng:0.9.0--h192cbe9_1
+docker pull quay.io/biocontainers/fasttree:2.1.11--h7b50bb2_5
+docker pull quay.io/staphb/iqtree2:2.4.0
+
 
 mkdir -p data
 mkdir -p results
