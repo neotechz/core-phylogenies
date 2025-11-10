8136a510b00a944ca65dcf76827a41ccb44068af
neotechz <marc.ayalde@gmail.com>
2025-11-10 11:26:23 +0800

    Modify test for REMOVE_INVARIABLE_SITES to use snp-sites container

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ docker pull quay.io/biocontainers/modeltest-ng:0.1.7--hf316886_3
 docker pull quay.io/biocontainers/raxml-ng:0.9.0--h192cbe9_1
 docker pull quay.io/biocontainers/fasttree:2.1.11--h7b50bb2_5
 docker pull quay.io/staphb/iqtree2:2.4.0
-
+docker pull quay.io/biocontainers/snp-sites:2.4.0--0
 
 mkdir -p data
 mkdir -p results
